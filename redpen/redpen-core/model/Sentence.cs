2a12af10878c465c171c42cfb72617ee4cd0f2be
aki426 <aki426@outlook.jp>
2024-03-08 10:11:46 +0900

    feat: Sentence.csの仮ベタ移植。

--- redpen/redpen-core/model/Sentence.cs
+++ redpen/redpen-core/model/Sentence.cs
@@ -3,302 +3,94 @@ using redpen_core.tokenizer;
 
 namespace redpen_core.model
 {
-    public class Sentence
+    /// <summary>Sentence block in a Document.</summary>
+    public record class Sentence
     {
+        // TODO: recordで実装したが、隠ぺいしたほうが良いものはアクセシビリティを変更する。
+
         private static readonly long serialVersionUID = 3761982769692999924L;
 
-        private readonly List<string> links;
-        private int lineNumber;
-        private string content;
-        private int startPositionOffset;
-        private bool isFirstSentence;
-        private List<TokenElement> tokens;
-        private List<LineOffset> offsetMap;
+        public string Content { get; init; } // need getter. setterはJAVA版で使用実績があるが必要に応じて対応する。
+        public int LineNumber { get; init; } // need getter. setterはJAVA版でも使用実績がないので不要。
+        public bool IsFirstSentence { get; init; } // need getter.
+        public List<string> Links { get; init; } // need getter. List.add()はJAVA版でも使用実績がある。
+        public List<TokenElement> Tokens { get; init; } // need getter. setterはJAVA版で使用実績があるが必要に応じて対応する。
+        public int StartPositionOffset { get; init; } // need getter. setterはJAVA版で使用実績があるが必要に応じて対応する。
+        public List<LineOffset> OffsetMap { get; init; }// need getter. setterはJAVA版で使用実績があるが必要に応じて対応する。
 
-        //    /**
-        //     * Links (including internal and external ones).
-        //     */
-        //    private final List<String> links;
-        //    /**
-        //     * Sentence position in a file.
-        //     */
-        //    private int lineNumber;
-        //    /**
-        //     * Content of string.
-        //     */
-        //    private String content;
-        //    /**
-        //     * Position which the sentence starts with.
-        //     */
-        //    private int startPositionOffset;
-        //    /**
-        //     * Flag for knowing if the sentence is the first sentence
-        //     * of a block, such as paragraph, list, header.
-        //     */
-        //    private boolean isFirstSentence;
-        //    /**
-        //     * A list of tokens.
-        //     * <p>
-        //     * Note: the contents of the tokens are added in DocumentCollectionBuilder
-        //     */
-        //    private List<TokenElement> tokens;
-        //    /**
-        //     * Combinations of line Number and the position offset
-        //     */
-        //    private List<LineOffset> offsetMap;
+        public Sentence(string sentenceContent, int lineNum) : this(sentenceContent, lineNum, 0)
+        {
+        }
 
-        //    /**
-        //     * Constructor.
-        //     *
-        //     * @param sentenceContent content of sentence
-        //     * @param lineNum         line number of sentence
-        //     */
-        //    public Sentence(String sentenceContent, int lineNum)
-        //    {
-        //        this(sentenceContent, lineNum, 0);
-        //    }
+        public Sentence(string sentenceContent, int lineNum, int startOffset)
+        {
+            this.Content = sentenceContent;
+            this.LineNumber = lineNum;
+            this.IsFirstSentence = false;
+            this.Links = new List<string>();
+            this.Tokens = new List<TokenElement>();
+            this.StartPositionOffset = startOffset;
+            this.OffsetMap = new List<LineOffset>();
+        }
 
-        //    /**
-        //     * Constructor.
-        //     *
-        //     * @param sentenceContent  content of sentence
-        //     * @param lineNum          line number of sentence
-        //     * @param startOffset      offset of the start position in the line
-        //     */
-        //    public Sentence(String sentenceContent, int lineNum, int startOffset)
-        //    {
-        //        super();
-        //        this.content = sentenceContent;
-        //        this.lineNumber = lineNum;
-        //        this.isFirstSentence = false;
-        //        this.links = new ArrayList<>();
-        //        this.tokens = new ArrayList<>();
-        //        this.startPositionOffset = startOffset;
-        //        this.offsetMap = new ArrayList<>();
-        //    }
+        public Sentence(string content, List<LineOffset> offsetMap, List<string> links)
+        {
+            this.Content = content;
+            this.LineNumber = offsetMap[0].LineNum;
+            this.IsFirstSentence = false;
+            this.Links = links;
+            this.Tokens = new List<TokenElement>();
+            this.StartPositionOffset = offsetMap[0].Offset;
+            this.OffsetMap = offsetMap;
+        }
 
-        //    public Sentence(String content, List<LineOffset> offsetMap, List<String> links)
-        //    {
-        //        this.content = content;
-        //        this.offsetMap = offsetMap;
-        //        this.startPositionOffset = offsetMap.get(0).offset;
-        //        this.lineNumber = offsetMap.get(0).lineNum;
-        //        this.isFirstSentence = false;
-        //        this.tokens = new ArrayList<>();
-        //        this.links = links;
-        //    }
+        /// <summary>
+        /// Get offset position for specified character position.
+        /// </summary>
+        /// <param name="position">character position in a sentence</param>
+        /// <returns>offset position</returns>
+        public LineOffset? GetOffset(int position)
+        {
+            if (position >= 0)
+            {
+                // MEMO: あらかじめContentの全文字とLineOffsetの対応関係を表現した
+                // List<LineOffset>が作成されており、Contentの文字数とList＜LineOffset>の要素数が同じに保たれていることが前提。
+                if (this.OffsetMap.Count > position)
+                {
+                    return this.OffsetMap[position];
+                }
+                else if ((position > 0) && (this.OffsetMap.Count == position))
+                {
+                    // MEMO: Contentが空文字列ではない場合の末尾指定。改行かEOLのためのもの？
+                    LineOffset last = this.OffsetMap[this.OffsetMap.Count - 1];
+                    return new LineOffset(last.LineNum, last.Offset + 1);
+                }
 
-        //    /**
-        //     * Get line number where the sentence starts.
-        //     *
-        //     * @return line number
-        //     */
-        //    public int getLineNumber()
-        //    {
-        //        return lineNumber;
-        //    }
+                // MEMO: Contentの長さを超えてLineOffsetを返すことになるはず。
+                return new LineOffset(this.LineNumber, position);
+            }
 
-        //    /**
-        //     * Set line number where the sentence starts.
-        //     *
-        //     * @param lineNumber line number
-        //     */
-        //    public void setLineNumber(int lineNumber)
-        //    {
-        //        this.lineNumber = lineNumber;
-        //    }
+            return null;
+        }
 
-        //    /**
-        //     * Return links the sentence contains.
-        //     *
-        //     * @return a set of links
-        //     */
-        //    public List<String> getLinks()
-        //    {
-        //        return links;
-        //    }
+        /// <summary>
+        /// Get the position of the supplied offset (ie: the position in the source text) in this sentence's normalized content
+        /// </summary>
+        /// <param name="offset">the position in the source text</param>
+        /// <returns>the position in the setence's content</returns>
+        public int GetOffsetPosition(LineOffset offset)
+        {
+            int position = this.OffsetMap.IndexOf(offset);
+            return position < 0 ? 0 : position;
+        }
 
-        //    /**
-        //     * Add a link to Sentence
-        //     *
-        //     * @param link link url
-        //     */
-        //    public void addLink(String link)
-        //    {
-        //        this.links.add(link);
-        //    }
-
-        //    /**
-        //     * Get content of sentence.
-        //     *
-        //     * @return sentence
-        //     */
-        //    public String getContent()
-        //    {
-        //        return content;
-        //    }
-
-        //    /**
-        //     * Set content of sentence.
-        //     *
-        //     * @param content sentence
-        //     */
-        //    public void setContent(String content)
-        //    {
-        //        this.content = content;
-        //    }
-
-        //    /**
-        //     * Get start column offset where the sentence starts.
-        //     *
-        //     * @return column offset of start sentence
-        //     */
-        //    public int getStartPositionOffset()
-        //    {
-        //        return startPositionOffset;
-        //    }
-
-        //    /**
-        //     * Detect the sentence is the first sentence of a paragraph.
-        //     *
-        //     * @return true when the first sentence of a paragraph. false otherwise.
-        //     */
-        //    public boolean isFirstSentence()
-        //    {
-        //        return isFirstSentence;
-        //    }
-
-        //    /**
-        //     * Set a flag to detect if the sentence is a first sentence of a paragraph.
-        //     *
-        //     * @param isFirstSentence a flag to detect if the sentence exists in the begging of a paragraph
-        //     */
-        //    public void setIsFirstSentence(boolean isFirstSentence)
-        //    {
-        //        this.isFirstSentence = isFirstSentence;
-        //    }
-
-        //    /**
-        //     * Get a set of tokenized words in the sentence.
-        //     *
-        //     * @return list of tokenized words
-        //     */
-        //    public List<TokenElement> getTokens()
-        //    {
-        //        return tokens;
-        //    }
-
-        //    /**
-        //     * Set a set of tokenized words.
-        //     *
-        //     * @param tokens tokenized words
-        //     */
-        //    public void setTokens(List<TokenElement> tokens)
-        //    {
-        //        this.tokens = tokens;
-        //    }
-
-        //    /**
-        //     * Set the offset mapping table which contains character position to column offset in line.
-        //     *
-        //     * @param offsetMap position mapping table
-        //     */
-        //    public void setOffsetMap(List<LineOffset> offsetMap)
-        //    {
-        //        this.offsetMap = offsetMap;
-        //    }
-
-        //    /**
-        //     * Get offset position for specified character position.
-        //     *
-        //     * @param position character position in a sentence
-        //     * @return offset position
-        //     */
-        //    public Optional<LineOffset> getOffset(int position)
-        //    {
-        //        if (position >= 0)
-        //        {
-        //            if (offsetMap.size() > position)
-        //            {
-        //                return Optional.of(offsetMap.get(position));
-        //            }
-        //            else if ((position > 0) && (offsetMap.size() == position))
-        //            {
-        //                LineOffset last = offsetMap.get(offsetMap.size() - 1);
-        //                return Optional.of(new LineOffset(last.lineNum, last.offset + 1));
-        //            }
-        //            return Optional.of(new LineOffset(lineNumber, position));
-        //        }
-        //        return Optional.empty();
-        //    }
-
-        //    /**
-        //     * Get the position of the supplied offset (ie: the position in the source text) in this sentence's normalized content
-        //     *
-        //     * @param offset the position in the source text
-        //     * @return the position in the setence's content
-        //     */
-        //    public int getOffsetPosition(LineOffset offset)
-        //    {
-        //        int position = offsetMap.indexOf(offset);
-        //        return position < 0 ? 0 : position;
-        //    }
-
-        //    /**
-        //     * Get size of offset mapping table (the size should be same as the content length).
-        //     *
-        //     * @return size of position mapping table
-        //     */
-        //    public int getOffsetMapSize()
-        //    {
-        //        return this.offsetMap.size();
-        //    }
-
-        //    @Override
-        //public String toString()
-        //    {
-        //        return "Sentence{" +
-        //                "links=" + links +
-        //                ", lineNumber=" + lineNumber +
-        //                ", content='" + content + '\'' +
-        //                ", startPositionOffset=" + startPositionOffset +
-        //                ", isFirstSentence=" + isFirstSentence +
-        //                ", tokens=" + tokens +
-        //                ", offsetMap=" + offsetMap +
-        //                '}';
-        //    }
-
-        //    @Override
-        //public boolean equals(Object o)
-        //    {
-        //        if (this == o) return true;
-        //        if (o == null || getClass() != o.getClass()) return false;
-
-        //        Sentence sentence = (Sentence)o;
-
-        //        if (isFirstSentence != sentence.isFirstSentence) return false;
-        //        if (lineNumber != sentence.lineNumber) return false;
-        //        if (startPositionOffset != sentence.startPositionOffset) return false;
-        //        if (content != null ? !content.equals(sentence.content) : sentence.content != null) return false;
-        //        if (links != null ? !links.equals(sentence.links) : sentence.links != null) return false;
-        //        if (tokens != null ? !tokens.equals(sentence.tokens) : sentence.tokens != null) return false;
-        //        return true;
-        //    }
-        //    @Override
-        //public int hashCode()
-        //    {
-        //        int result = links != null ? links.hashCode() : 0;
-        //        result = 31 * result + lineNumber;
-        //        result = 31 * result + (content != null ? content.hashCode() : 0);
-        //        result = 31 * result + startPositionOffset;
-        //        result = 31 * result + (isFirstSentence ? 1 : 0);
-        //        result = 31 * result + (tokens != null ? tokens.hashCode() : 0);
-        //        return result;
-        //    }
-
-        //    public List<LineOffset> getOffsetMap()
-        //    {
-        //        return offsetMap;
-        //    }
+        /// <summary>
+        /// Get size of offset mapping table (the size should be same as the content length).
+        /// </summary>
+        /// <returns>size of position mapping table</returns>
+        public int GetOffsetMapSize()
+        {
+            return this.OffsetMap.Count;
+        }
     }
 }
