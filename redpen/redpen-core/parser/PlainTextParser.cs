7c225c34e8494d7fe795691d7e7d00a47f7b031c
aki426 <aki426@outlook.jp>
2024-03-10 18:31:14 +0900

    style: 未使用Usingを除去。

--- redpen/redpen-core/parser/PlainTextParser.cs
+++ redpen/redpen-core/parser/PlainTextParser.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NLog.LayoutRenderers;
-using redpen_core.model;
+﻿using redpen_core.model;
 using redpen_core.tokenizer;
 
 namespace redpen_core.parser
