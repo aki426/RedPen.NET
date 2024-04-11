6d3707c2b0e50cdf0d2355db1fca077bb62240e5
aki426 <aki426@outlook.jp>
2024-04-12 07:25:55 +0900

    feat: SentenceLengthのConfigurationを追加。MaxLengthパラメータを表すインターフェース型を追加。

---
+++ RedPen.Net.Core/Validators/SentenceValidator/SentenceLengthConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using RedPen.Net.Core.Config;
+
+namespace RedPen.Net.Core.Validators.SentenceValidator
+{
+    public record SentenceLengthConfiguration : ValidatorConfiguration, IMaxLengthConfigParameter
+    {
+        public int MaxLength { get; init; }
+
+        public SentenceLengthConfiguration(string level, int maxLength) : base(level)
+        {
+            MaxLength = maxLength;
+        }
+        public SentenceLengthConfiguration(ValidationLevel level, int maxLength) : base(level)
+        {
+            MaxLength = maxLength;
+        }
+    }
+}
