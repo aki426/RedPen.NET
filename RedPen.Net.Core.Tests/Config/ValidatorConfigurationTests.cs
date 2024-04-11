d8abe9686a6cead2b66fe8eab44eab38f3f8e1b6
aki426 <aki426@outlook.jp>
2024-04-12 07:16:12 +0900

    feat: ValidatorConfigurationをrecord型に変更。 test: ValidatorConfigurationのテストケースを修正。

--- RedPen.Net.Core.Tests/Config/ValidatorConfigurationTests.cs
+++ RedPen.Net.Core.Tests/Config/ValidatorConfigurationTests.cs
@@ -1,5 +1,4 @@
-﻿using FluentAssertions;
-using RedPen.Net.Core.Config;
+﻿using RedPen.Net.Core.Config;
 using Xunit;
 using Assert = Xunit.Assert;
 
@@ -10,52 +9,19 @@ namespace RedPen.Net.Core.Tests.Config
         [Fact()]
         public void CloneTest()
         {
-            ValidatorConfiguration conf = new ValidatorConfiguration("test").AddProperty("foo", "bar");
-            ValidatorConfiguration clone = conf.Clone();
+            ValidatorConfiguration conf = new ValidatorConfiguration("ERROR");
+            ValidatorConfiguration clone = conf;
 
             // XUnitでの等価性と同一性の検証
             Assert.NotSame(conf, clone);
             Assert.Equal(conf, clone);
 
-            Assert.Equal(conf.ConfigurationName, clone.ConfigurationName);
-
-            Assert.NotSame(conf.Properties, clone.Properties);
-            Assert.Equal(conf.Properties, clone.Properties);
-
+            // プロパティの等価性と同一性の検証
             Assert.Equal(conf.Level, clone.Level);
-        }
+            Assert.NotSame(conf.Level, clone.Level);
 
-        [Fact]
-        public void EqualsTest()
-        {
-            ValidatorConfiguration conf = new ValidatorConfiguration("test").AddProperty("foo", "bar").SetLevel(ValidationLevel.ERROR);
-            ValidatorConfiguration conf2 = new ValidatorConfiguration("test").AddProperty("foo", "bar").SetLevel(ValidationLevel.ERROR);
-
-            Assert.Equal(conf, conf2);
-        }
-
-        [Fact]
-        public void EqualsNamesTest()
-        {
-            ValidatorConfiguration conf = new ValidatorConfiguration("test");
-            ValidatorConfiguration conf2 = new ValidatorConfiguration("test2");
-            conf.Equals(conf2).Should().BeFalse();
-        }
-
-        [Fact]
-        public void EqualsPropertiesTest()
-        {
-            ValidatorConfiguration conf = new ValidatorConfiguration("test").AddProperty("foo", "bar");
-            ValidatorConfiguration conf2 = new ValidatorConfiguration("test").AddProperty("foo", "bar2");
-            conf.Equals(conf2).Should().BeFalse();
-        }
-
-        [Fact]
-        public void EqualsLevelsTest()
-        {
-            ValidatorConfiguration conf = new ValidatorConfiguration("test").SetLevel(ValidationLevel.INFO);
-            ValidatorConfiguration conf2 = new ValidatorConfiguration("test").SetLevel(ValidationLevel.WARN);
-            conf.Equals(conf2).Should().BeFalse();
+            // with式はC#9で利用可能。
+            //ValidatorConfiguration changedLevel = conf with { Level = ValidationLevel.INFO };
         }
     }
 }
--- RedPen.Net.Core/Config/ValidatorConfiguration.cs
+++ RedPen.Net.Core/Config/ValidatorConfiguration.cs
@@ -4,196 +4,207 @@ using System.Linq;
 
 namespace RedPen.Net.Core.Config
 {
-    /// <summary>
-    /// The validator configuration.
-    /// </summary>
-    public class ValidatorConfiguration : ICloneable, IEquatable<ValidatorConfiguration>
+    public record ValidatorConfiguration(ValidationLevel Level = ValidationLevel.OFF)
     {
-        // TODO: むしろValidationNameなどに改めた方が良い。移植の都合上Ver 1.0まではこのままとする。
-        /// <summary>Configuration name string</summary>
-        public string ConfigurationName { get; private set; }
-
-        /// <summary>Property dictionary</summary>
-        public Dictionary<string, string> Properties { get; private set; }
-
-        /// <summary>エラーLevel。デフォルトはERROR</summary>
-        public ValidationLevel Level { get; private set; } = ValidationLevel.ERROR;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="ValidatorConfiguration"/> class.
         /// </summary>
-        /// <param name="configurationName">The configuration name.</param>
-        public ValidatorConfiguration(string configurationName) :
-            this(configurationName, new Dictionary<string, string>())
-        {
-        }
+        /// <param name="level">ValidationLevelのテキスト表現</param>
+        public ValidatorConfiguration(string level) : this(ValidationLevelExtend.ConvertFrom(level)) { }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ValidatorConfiguration"/> class.
-        /// </summary>
-        /// <param name="configurationName">The configuration name.</param>
-        /// <param name="properties">The properties.</param>
-        public ValidatorConfiguration(string configurationName, Dictionary<string, string> properties) :
-            this(configurationName, properties, ValidationLevel.ERROR)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ValidatorConfiguration"/> class.
-        /// </summary>
-        /// <param name="configurationName">The configuration name.</param>
-        /// <param name="properties">The properties.</param>
-        /// <param name="level">The level.</param>
-        public ValidatorConfiguration(string configurationName, Dictionary<string, string> properties, ValidationLevel level)
-        {
-            this.ConfigurationName = configurationName;
-            this.Properties = properties;
-            this.Level = level;
-        }
-
-        // MEMO: JAVAの実装ではこのメソッドが存在したが、C#ではPropertiesを公開しているので呼び出し側で対応する。
-        ///// <summary>
-        ///// Gets the property.
-        ///// </summary>
-        ///// <param name="name">The name.</param>
-        ///// <returns>A string.</returns>
-        //public string GetProperty(string name)
-        //{
-        //    // TODO: Dictionaryのキーに対するアクセスはTryGetValueを使うべき。
-        //    return this.Properties[name];
-        //}
-
-        /// <summary>
-        /// Sets the level.
-        /// </summary>
-        /// <param name="level">The level.</param>
-        /// <returns>A ValidatorConfiguration.</returns>
-        public ValidatorConfiguration SetLevel(string level)
-        {
-            try
-            {
-                this.Level = ValidationLevelExtend.ConvertFrom(level);
-            }
-            catch (ArgumentException e)
-            {
-                // MEMO: JAVA実装ではRuntimeExceptionに変更して再throwしているが、C#ではそのような例外は存在しない。
-                throw;
-            }
-            return this;
-        }
-
-        /// <summary>
-        /// Sets the level.
-        /// </summary>
-        /// <param name="level">The level.</param>
-        /// <returns>A ValidatorConfiguration.</returns>
-        public ValidatorConfiguration SetLevel(ValidationLevel level)
-        {
-            this.Level = level;
-            return this;
-        }
-
-        /// <summary>
-        /// Gets the validator class name.
-        /// </summary>
-        /// <returns>A string.</returns>
-        public string GetValidatorClassName()
-        {
-            return ConfigurationName + "Validator";
-        }
-
-        /// <summary>
-        /// Adds the property.
-        /// </summary>
-        /// <param name="name">The name.</param>
-        /// <param name="value">The value.</param>
-        /// <returns>A ValidatorConfiguration.</returns>
-        public ValidatorConfiguration AddProperty(string name, object value)
-        {
-            // JAVAの実装ではObject型に対してString.valueOfを使っている。一旦ToStringを使う。
-            Properties.Add(name, value.ToString());
-            return this;
-        }
-
-        // MEMO: JAVAの実装ではaddAttributeがあったが使用されていなかったので実装しない。
-        // 中身はaddPropertyと同じだった。
-
-        /// <summary>
-        /// Equals the.
-        /// </summary>
-        /// <param name="obj">The obj.</param>
-        /// <returns>A bool.</returns>
-        public override bool Equals(object? obj)
-        {
-            if (this == obj) return true;
-            if (obj == null || obj is not ValidatorConfiguration) return false;
-
-            return this.Equals(obj as ValidatorConfiguration);
-        }
-
-        /// <summary>
-        /// Equals the.
-        /// </summary>
-        /// <param name="other">The other.</param>
-        /// <returns>A bool.</returns>
-        public bool Equals(ValidatorConfiguration? other)
-        {
-            if (this == other) return true;
-            if (other == null) return false;
-
-            // JAVA版では同値性判定にObjects.equalsを使っているが、これはnull許容型に対しても使えるためと思われる。
-            return ConfigurationName == other.ConfigurationName &&
-                   Properties.SequenceEqual(other.Properties) &&
-                   Level == other.Level;
-        }
-
-        /// <summary>
-        /// Gets the hash code.
-        /// </summary>
-        /// <returns>An int.</returns>
-        public override int GetHashCode()
-        {
-            // ConfigurationNameのみのHash値を返す。
-            return HashCode.Combine(ConfigurationName);
-        }
-
-        /// <summary>
-        /// Tos the string.
-        /// </summary>
-        /// <returns>A string.</returns>
-        public override string ToString()
-        {
-            return ConfigurationName;
-        }
-
-        /// <summary>
-        /// Clones the.
-        /// </summary>
-        /// <returns>A ValidatorConfiguration.</returns>
-        public ValidatorConfiguration Clone()
-        {
-            try
-            {
-                return new ValidatorConfiguration(
-                    this.ConfigurationName,
-                    new Dictionary<string, string>(this.Properties),
-                    this.Level);
-            }
-            catch (Exception e)
-            {
-                // JAVA版ではCloneNotSupportedExceptionをRuntimeExceptionにラップして再throwしているが、
-                // C#では暫定的にExceptionの再スローとした。
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// Clones the.
-        /// </summary>
-        /// <returns>An object.</returns>
-        object ICloneable.Clone()
-        {
-            return this.Clone();
-        }
+        // public string GetValidatorClassName() => ConfigurationName + "Validator";
     }
+
+    ///// <summary>
+    ///// The validator configuration.
+    ///// </summary>
+    //public class ValidatorConfigurationOld : ICloneable, IEquatable<ValidatorConfiguration>
+    //{
+    //    // TODO: むしろValidationNameなどに改めた方が良い。移植の都合上Ver 1.0まではこのままとする。
+    //    /// <summary>Configuration name string</summary>
+    //    public string ConfigurationName { get; private set; }
+
+    //    /// <summary>Property dictionary</summary>
+    //    public Dictionary<string, string> Properties { get; private set; }
+
+    //    /// <summary>エラーLevel。デフォルトはERROR</summary>
+    //    public ValidationLevel Level { get; private set; } = ValidationLevel.ERROR;
+
+    //    /// <summary>
+    //    /// Initializes a new instance of the <see cref="ValidatorConfiguration"/> class.
+    //    /// </summary>
+    //    /// <param name="configurationName">The configuration name.</param>
+    //    public ValidatorConfiguration(string configurationName) :
+    //        this(configurationName, new Dictionary<string, string>())
+    //    {
+    //    }
+
+    //    /// <summary>
+    //    /// Initializes a new instance of the <see cref="ValidatorConfiguration"/> class.
+    //    /// </summary>
+    //    /// <param name="configurationName">The configuration name.</param>
+    //    /// <param name="properties">The properties.</param>
+    //    public ValidatorConfiguration(string configurationName, Dictionary<string, string> properties) :
+    //        this(configurationName, properties, ValidationLevel.ERROR)
+    //    {
+    //    }
+
+    //    /// <summary>
+    //    /// Initializes a new instance of the <see cref="ValidatorConfiguration"/> class.
+    //    /// </summary>
+    //    /// <param name="configurationName">The configuration name.</param>
+    //    /// <param name="properties">The properties.</param>
+    //    /// <param name="level">The level.</param>
+    //    public ValidatorConfiguration(string configurationName, Dictionary<string, string> properties, ValidationLevel level)
+    //    {
+    //        this.ConfigurationName = configurationName;
+    //        this.Properties = properties;
+    //        this.Level = level;
+    //    }
+
+    //    // MEMO: JAVAの実装ではこのメソッドが存在したが、C#ではPropertiesを公開しているので呼び出し側で対応する。
+    //    ///// <summary>
+    //    ///// Gets the property.
+    //    ///// </summary>
+    //    ///// <param name="name">The name.</param>
+    //    ///// <returns>A string.</returns>
+    //    //public string GetProperty(string name)
+    //    //{
+    //    //    // TODO: Dictionaryのキーに対するアクセスはTryGetValueを使うべき。
+    //    //    return this.Properties[name];
+    //    //}
+
+    //    /// <summary>
+    //    /// Sets the level.
+    //    /// </summary>
+    //    /// <param name="level">The level.</param>
+    //    /// <returns>A ValidatorConfiguration.</returns>
+    //    public ValidatorConfiguration SetLevel(string level)
+    //    {
+    //        try
+    //        {
+    //            this.Level = ValidationLevelExtend.ConvertFrom(level);
+    //        }
+    //        catch (ArgumentException e)
+    //        {
+    //            // MEMO: JAVA実装ではRuntimeExceptionに変更して再throwしているが、C#ではそのような例外は存在しない。
+    //            throw;
+    //        }
+    //        return this;
+    //    }
+
+    //    /// <summary>
+    //    /// Sets the level.
+    //    /// </summary>
+    //    /// <param name="level">The level.</param>
+    //    /// <returns>A ValidatorConfiguration.</returns>
+    //    public ValidatorConfiguration SetLevel(ValidationLevel level)
+    //    {
+    //        this.Level = level;
+    //        return this;
+    //    }
+
+    //    /// <summary>
+    //    /// Gets the validator class name.
+    //    /// </summary>
+    //    /// <returns>A string.</returns>
+    //    public string GetValidatorClassName()
+    //    {
+    //        return ConfigurationName + "Validator";
+    //    }
+
+    //    /// <summary>
+    //    /// Adds the property.
+    //    /// </summary>
+    //    /// <param name="name">The name.</param>
+    //    /// <param name="value">The value.</param>
+    //    /// <returns>A ValidatorConfiguration.</returns>
+    //    public ValidatorConfiguration AddProperty(string name, object value)
+    //    {
+    //        // JAVAの実装ではObject型に対してString.valueOfを使っている。一旦ToStringを使う。
+    //        Properties.Add(name, value.ToString());
+    //        return this;
+    //    }
+
+    //    // MEMO: JAVAの実装ではaddAttributeがあったが使用されていなかったので実装しない。
+    //    // 中身はaddPropertyと同じだった。
+
+    //    /// <summary>
+    //    /// Equals the.
+    //    /// </summary>
+    //    /// <param name="obj">The obj.</param>
+    //    /// <returns>A bool.</returns>
+    //    public override bool Equals(object? obj)
+    //    {
+    //        if (this == obj) return true;
+    //        if (obj == null || obj is not ValidatorConfiguration) return false;
+
+    //        return this.Equals(obj as ValidatorConfiguration);
+    //    }
+
+    //    /// <summary>
+    //    /// Equals the.
+    //    /// </summary>
+    //    /// <param name="other">The other.</param>
+    //    /// <returns>A bool.</returns>
+    //    public bool Equals(ValidatorConfiguration? other)
+    //    {
+    //        if (this == other) return true;
+    //        if (other == null) return false;
+
+    //        // JAVA版では同値性判定にObjects.equalsを使っているが、これはnull許容型に対しても使えるためと思われる。
+    //        return ConfigurationName == other.ConfigurationName &&
+    //               Properties.SequenceEqual(other.Properties) &&
+    //               Level == other.Level;
+    //    }
+
+    //    /// <summary>
+    //    /// Gets the hash code.
+    //    /// </summary>
+    //    /// <returns>An int.</returns>
+    //    public override int GetHashCode()
+    //    {
+    //        // ConfigurationNameのみのHash値を返す。
+    //        return HashCode.Combine(ConfigurationName);
+    //    }
+
+    //    /// <summary>
+    //    /// Tos the string.
+    //    /// </summary>
+    //    /// <returns>A string.</returns>
+    //    public override string ToString()
+    //    {
+    //        return ConfigurationName;
+    //    }
+
+    //    /// <summary>
+    //    /// Clones the.
+    //    /// </summary>
+    //    /// <returns>A ValidatorConfiguration.</returns>
+    //    public ValidatorConfiguration Clone()
+    //    {
+    //        try
+    //        {
+    //            return new ValidatorConfiguration(
+    //                this.ConfigurationName,
+    //                new Dictionary<string, string>(this.Properties),
+    //                this.Level);
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            // JAVA版ではCloneNotSupportedExceptionをRuntimeExceptionにラップして再throwしているが、
+    //            // C#では暫定的にExceptionの再スローとした。
+    //            throw;
+    //        }
+    //    }
+
+    //    /// <summary>
+    //    /// Clones the.
+    //    /// </summary>
+    //    /// <returns>An object.</returns>
+    //    object ICloneable.Clone()
+    //    {
+    //        return this.Clone();
+    //    }
+    //}
 }
