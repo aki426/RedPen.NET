a83b83486b6b59499e711dc8608f1d3d42cada5b
aki426 <aki426@outlook.jp>
2024-04-15 09:45:10 +0900

    feat: ファイル追加。

---
+++ RedPen.Net.Core/Tokenizer/RedPenTokenizerFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace RedPen.Net.Core.Tokenizer
+{
+    /// <summary>
+    /// TokenizerのFactoryパターン。
+    /// </summary>
+    public static class RedPenTokenizerFactory
+    {
+        /// <summary>
+        /// Creates the tokenizer.
+        /// </summary>
+        /// <param name="cultureInfo">The culture info.</param>
+        /// <returns>An IRedPenTokenizer.</returns>
+        public static IRedPenTokenizer CreateTokenizer(CultureInfo cultureInfo)
+        {
+            return cultureInfo switch
+            {
+                // 日本語の場合は、NeologdJapaneseTokenizerを返す
+                CultureInfo culture when culture.Name == "ja-JP" => new NeologdJapaneseTokenizer(),
+                // その他の言語の場合は、WhiteSpaceTokenizerを返す。英語をはじめとして圧倒的に半角スペースで区切る言語が多いため。
+                _ => new WhiteSpaceTokenizer()
+            };
+        }
+    }
+}
---
+++ RedPen.Net.Core/Validators/DocumentValidator/JapaneseExpressionVariationConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using RedPen.Net.Core.Config;
+
+namespace RedPen.Net.Core.Validators.DocumentValidator
+{
+    public record JapaneseExpressionVariationConfiguration : ValidatorConfiguration, IWordMapConfigParameter
+    {
+        public Dictionary<string, string> WordMap { get; init; }
+    }
+}
