93fe37ef14d07c70f47a7c968350c02ddb401296
aki426 <aki426@outlook.jp>
2024-04-23 20:26:27 +0900

    feat: SeggestExpressionを実装するためのファイルを追加。

---
+++ RedPen.Net.Core/Validators/SentenceValidator/SuggestExpressionConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using RedPen.Net.Core.Config;
+
+namespace RedPen.Net.Core.Validators.SentenceValidator
+{
+    public record SuggestExpressionConfiguration : ValidatorConfiguration, IWordMapConfigParameter
+    {
+        public Dictionary<string, string> WordMap { get; init; }
+
+        public SuggestExpressionConfiguration(ValidationLevel level, Dictionary<string, string> wordMap) : base(level)
+        {
+            WordMap = wordMap;
+        }
+    }
+}
---
+++ RedPen.Net.Core/Validators/SentenceValidator/SuggestExpressionValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using NLog;
+using RedPen.Net.Core.Config;
+using RedPen.Net.Core.Model;
+
+namespace RedPen.Net.Core.Validators.SentenceValidator
+{
+    public class SuggestExpressionValidator : Validator, ISentenceValidatable
+    {
+        /// <summary>Nlog</summary>
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>ValidationConfiguration</summary>
+        public SuggestExpressionConfiguration Config { get; init; }
+
+        public SuggestExpressionValidator(
+            ValidationLevel level,
+            CultureInfo documentLangForTest,
+            SymbolTable symbolTable,
+            SuggestExpressionConfiguration config) : base(
+                config.Level,
+                documentLangForTest,
+                symbolTable)
+        {
+            this.Config = config;
+        }
+
+        public void PreValidate(Sentence sentence)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ValidationError> Validate(Sentence sentence)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
