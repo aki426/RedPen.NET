2b2477b1fed7b8bcab5ab9f97623d9c435d1c4cf
aki426 <aki426@outlook.jp>
2024-04-25 00:33:42 +0900

    作業途中コミット。

--- RedPen.Net.Core/Validators/SentenceValidator/JapaneseStyleValidator.cs
+++ RedPen.Net.Core/Validators/SentenceValidator/JapaneseStyleValidator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
+using System.Text.RegularExpressions;
 using NLog;
 using RedPen.Net.Core.Config;
 using RedPen.Net.Core.Model;
@@ -38,9 +39,17 @@ namespace RedPen.Net.Core.Validators.SentenceValidator
             this.Config = config;
         }
 
+        private static readonly Regex DearuPattern = new Regex(@"である|のだが|であった|あるが|あった|だった");
+        private static readonly Regex DesumasuPattern = new Regex(@"ですね|でした|ました|でしたが|でしたので|ですので|ですが|です|ます");
+
+        private int dearuCount = 0;
+        private int desumasuCount = 0;
+
         public void PreValidate(Sentence sentence)
         {
-            // nothing.
+            // コンテンツとのマッチを数える
+            dearuCount += CountMatch(sentence, DearuPattern);
+            desumasuCount += CountMatch(sentence, DesumasuPattern);
         }
 
         public List<ValidationError> Validate(Sentence sentence)
@@ -48,6 +57,17 @@ namespace RedPen.Net.Core.Validators.SentenceValidator
             List<ValidationError> result = new List<ValidationError>();
 
             //// validation
+            ///
+            var forceDearu = GetBoolean("ForceDearu");
+
+            if (dearuCount > desumasuCount || forceDearu)
+            {
+                DetectPattern(sentence, DesumasuPattern);
+            }
+            else
+            {
+                DetectPattern(sentence, DearuPattern);
+            }
 
             //// TODO: MessageKey引数はErrorMessageにバリエーションがある場合にValidator内で条件判定して引数として与える。
             //result.Add(new ValidationError(
@@ -57,6 +77,22 @@ namespace RedPen.Net.Core.Validators.SentenceValidator
             //    MessageArgs: new object[] { argsForMessageArg }));
 
             return result;
+        }
+
+        private int CountMatch(Sentence sentence, Regex pattern)
+        {
+            var content = sentence.Content;
+            var matches = pattern.Matches(content);
+            return matches.Count;
+        }
+
+        private void DetectPattern(Sentence sentence, Regex pattern)
+        {
+            var matches = pattern.Matches(sentence.Content);
+            foreach (Match match in matches)
+            {
+                AddLocalizedErrorWithPosition(sentence, match.Index, match.Index + match.Length, match.Value);
+            }
         }
     }
 }
