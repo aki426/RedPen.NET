600f7e8f92d8064b0a415091cecca42b33304513
aki426 <aki426@outlook.jp>
2024-04-29 07:51:34 +0900

    feat: HankakuKanaの機能を追加するためにファイルを追加。

---
+++ RedPen.Net.Core/Validators/SentenceValidator/HankakuKanaValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedPen.Net.Core.Validators.SentenceValidator
+{
+    internal class HankakuKanaValidator
+    {
+    }
+}
