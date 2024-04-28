b0448c5353ade469d66a8c5cd861a6b62c0ec0e8
aki426 <aki426@outlook.jp>
2024-04-28 18:36:06 +0900

    feat: OkuriganaValidationに必要なファイルを追加。

---
+++ RedPen.Net.Core/Validators/SentenceValidator/OkuriganaValidator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using NLog;
+using RedPen.Net.Core.Config;
+using RedPen.Net.Core.Model;
+
+namespace RedPen.Net.Core.Validators.SentenceValidator
+{
+    /// <summary>OkuriganaのConfiguration</summary>
+    public record OkuriganaConfiguration : ValidatorConfiguration
+    {
+        public OkuriganaConfiguration(ValidationLevel level) : base(level)
+        {
+        }
+    }
+
+    // TODO: Validation対象に応じて、IDocumentValidatable, ISectionValidatable, ISentenceValidatableを実装する。
+    public class OkuriganaValidator : Validator, ISentenceValidatable
+    {
+        /// <summary>Nlog</summary>
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        // TODO: 専用のValidatorConfigurationを別途定義する。
+
+        public OkuriganaConfiguration Config { get; init; }
+
+        // TODO: サポート対象言語がANYではない場合overrideで再定義する。
+        /// <summary></summary>
+        public override List<string> SupportedLanguages => new List<string>() { "ja-JP" };
+
+        // TODO: コンストラクタの引数定義は共通にすること。
+        public OkuriganaValidator(
+            CultureInfo documentLangForTest,
+            SymbolTable symbolTable,
+            OkuriganaConfiguration config) :
+            base(
+                config.Level,
+                documentLangForTest,
+                symbolTable)
+        {
+            this.Config = config;
+        }
+
+        public List<ValidationError> Validate(Sentence sentence)
+        {
+            List<ValidationError> result = new List<ValidationError>();
+
+            // validation
+
+            // TODO: MessageKey引数はErrorMessageにバリエーションがある場合にValidator内で条件判定して引数として与える。
+            result.Add(new ValidationError(
+                ValidationType.Okurigana,
+                this.Level,
+                sentence,
+                MessageArgs: new object[] { "some result" }));
+
+            return result;
+        }
+    }
+}
