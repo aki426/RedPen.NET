602f922971a997e29eae1a09024d8eaf3eb02e4e
aki426 <aki426@outlook.jp>
2024-04-28 18:05:04 +0900

    feat: TaigendomeValidatorを実装。 test: TaigendomeValidatorTestsを実装。グリーンバー。

--- RedPen.Net.Core.Tests/Validator/SentenceValidator/TaigendomeValidatorTests.cs
+++ RedPen.Net.Core.Tests/Validator/SentenceValidator/TaigendomeValidatorTests.cs
@@ -1,12 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FluentAssertions;
+using RedPen.Net.Core.Config;
+using RedPen.Net.Core.Errors;
+using RedPen.Net.Core.Model;
+using RedPen.Net.Core.Tokenizer;
+using RedPen.Net.Core.Validators.SentenceValidator;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace RedPen.Net.Core.Tests.Validator.SentenceValidator
 {
     public class TaigendomeValidatorTests
     {
+        private readonly ITestOutputHelper output;
+
+        public TaigendomeValidatorTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Theory]
+        [InlineData("001", "吾輩は猫。", 1, "猫")]
+        [InlineData("002", "吾輩は猫だ。", 0, "")]
+        [InlineData("003", "ねこですよろしくおねがいします", 0, "")]
+        [InlineData("004", "これは体言止め？", 1, "止め")]
+        [InlineData("005", "今日の天気は晴れ。", 1, "晴れ")]
+        [InlineData("006", "今日の天気は晴れです。", 0, "")]
+        public void BasicTest(string nouse1, string text, int errorCount, string expected)
+        {
+            var validatorConfiguration = new TaigendomeConfiguration(ValidationLevel.ERROR);
+            CultureInfo documentLang = CultureInfo.GetCultureInfo("ja-JP");
+
+            // カスタムシンボルを使わない場合は空リストを渡す。デフォルトシンボルはnew時に自動的にSymbolTableにロードされる。
+            SymbolTable symbolTable = new SymbolTable(documentLang, "", new List<Symbol>());
+
+            // Validatorの生成。
+            var validator = new TaigendomeValidator(
+                documentLang,
+                symbolTable,
+                validatorConfiguration);
+
+            // Tokenizeが走るので大仰でもDocumentを生成する。
+            Document document = Document.Builder(RedPenTokenizerFactory.CreateTokenizer(documentLang))
+                    .AddSection(1)
+                    .AddParagraph()
+                    .AddSentence(new Sentence(text, 1))
+                    .Build(); // TokenizeをBuild時に実行する。
+
+            var sentence = document.Sections[0].Paragraphs[0].Sentences[0];
+
+            output.WriteLine("★全Token:");
+            foreach (var token in sentence.Tokens)
+            {
+                output.WriteLine(token.ToString());
+            }
+
+            // Validation
+            var errors = validator.Validate(sentence);
+
+            errors.Should().HaveCount(errorCount);
+
+            if (errors.Any())
+            {
+                var manager = ErrorMessageManager.GetInstance();
+
+                output.WriteLine("");
+                output.WriteLine("★Errors:");
+                foreach (var error in errors)
+                {
+                    output.WriteLine(error.ToString());
+                    output.WriteLine(manager.GetErrorMessage(error, CultureInfo.GetCultureInfo("ja-JP")));
+                }
+
+                string.Join(",", errors.Select(e => e.MessageArgs[0].ToString())).Should().Be(expected);
+            }
+        }
     }
 }
--- RedPen.Net.Core/Validators/SentenceValidator/TaigendomeValidator.cs
+++ RedPen.Net.Core/Validators/SentenceValidator/TaigendomeValidator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using NLog;
 using RedPen.Net.Core.Config;
 using RedPen.Net.Core.Model;
@@ -16,21 +17,19 @@ namespace RedPen.Net.Core.Validators.SentenceValidator
         }
     }
 
-    // TODO: Validation対象に応じて、IDocumentValidatable, ISectionValidatable, ISentenceValidatableを実装する。
+    /// <summary>TaigendomeのValidator</summary>
     public class TaigendomeValidator : Validator, ISentenceValidatable
     {
         /// <summary>Nlog</summary>
         private static Logger log = LogManager.GetCurrentClassLogger();
 
-        // TODO: 専用のValidatorConfigurationを別途定義する。
-
         public TaigendomeConfiguration Config { get; init; }
 
-        // TODO: サポート対象言語がANYではない場合overrideで再定義する。
+        // MEMO: サポート対象言語がANYではない場合overrideで再定義する。
         /// <summary></summary>
         public override List<string> SupportedLanguages => new List<string>() { "ja-JP" };
 
-        // TODO: コンストラクタの引数定義は共通にすること。
+        // MEMO: コンストラクタの引数定義は共通にすること。
         public TaigendomeValidator(
             CultureInfo documentLangForTest,
             SymbolTable symbolTable,
@@ -47,14 +46,23 @@ namespace RedPen.Net.Core.Validators.SentenceValidator
         {
             List<ValidationError> result = new List<ValidationError>();
 
-            // validation
-
-            // TODO: MessageKey引数はErrorMessageにバリエーションがある場合にValidator内で条件判定して引数として与える。
-            //result.Add(new ValidationError(
-            //    ValidationType.Taigendome,
-            //    this.Level,
-            //    sentence,
-            //    MessageArgs: new object[] { argsForMessageArg }));
+            // Tokenがない場合は体言止めではないと判断する。
+            if (sentence.Tokens.Any())
+            {
+                // 記号を除いた最後のTokenが名詞だった場合のみ体言止め表現とみなす。
+                var noSymbols = sentence.Tokens.Where(t => t.Tags[0] is not "記号").ToList();
+                if (noSymbols.Any() && (noSymbols.Last().Tags[0] is "名詞"))
+                {
+                    // TODO: MessageKey引数はErrorMessageにバリエーションがある場合にValidator内で条件判定して引数として与える。
+                    result.Add(new ValidationError(
+                        ValidationType.Taigendome,
+                        this.Level,
+                        sentence,
+                        noSymbols.Last().OffsetMap[0],
+                        noSymbols.Last().OffsetMap[^1],
+                        MessageArgs: new object[] { noSymbols.Last().Surface }));
+                }
+            }
 
             return result;
         }
