e7d2fc22a3cf6fc41ef1d80f0c275113c6b0eda0
aki426 <aki426@outlook.jp>
2024-05-01 15:00:47 +0900

    feat: SpaceBetweenAlphabeticalWordのためのファイルを追加。ValidatorTestsUtilityの追加忘れに対応。

---
+++ RedPen.Net.Core.Tests/Validator/SentenceValidator/ValidatorTestsUtility.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+using System.Linq;
+using FluentAssertions;
+using RedPen.Net.Core.Errors;
+using RedPen.Net.Core.Model;
+using RedPen.Net.Core.Tokenizer;
+using RedPen.Net.Core.Validators;
+using Xunit.Abstractions;
+
+namespace RedPen.Net.Core.Tests.Validator.SentenceValidator
+{
+    public static class ValidatorTestsUtility
+    {
+        public static void CommonSentenceErrorPatternTest(
+            ISentenceValidatable validator,
+            string text,
+            CultureInfo documentLang,
+            int errorCount,
+            string expected,
+            ITestOutputHelper output)
+        {
+            // Tokenizeが走るので大仰でもDocumentを生成する。
+            Document document = Document.Builder(RedPenTokenizerFactory.CreateTokenizer(documentLang))
+                    .AddSection(1)
+                    .AddParagraph()
+                    .AddSentence(new Sentence(text, 1))
+                    .Build(); // TokenizeをBuild時に実行する。
+
+            var sentence = document.Sections[0].Paragraphs[0].Sentences[0];
+
+            output.WriteLine("★全Token:");
+            foreach (var token in sentence.Tokens)
+            {
+                output.WriteLine(token.ToString());
+            }
+
+            // Validation
+            var errors = validator.Validate(sentence);
+
+            errors.Should().HaveCount(errorCount);
+
+            if (errors.Any())
+            {
+                var manager = ErrorMessageManager.GetInstance();
+
+                output.WriteLine("");
+                output.WriteLine("★Errors:");
+                foreach (var error in errors)
+                {
+                    output.WriteLine(error.ToString());
+                    output.WriteLine(manager.GetErrorMessage(error, CultureInfo.GetCultureInfo("ja-JP")));
+                }
+
+                string.Join(",", errors.Select(e => e.MessageArgs[0].ToString())).Should().Be(expected);
+            }
+        }
+    }
+}
---
+++ RedPen.Net.Core/Validators/SentenceValidator/SpaceBetweenAlphabeticalWordValidator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using NLog;
+using RedPen.Net.Core.Config;
+using RedPen.Net.Core.Model;
+
+namespace RedPen.Net.Core.Validators.SentenceValidator
+{
+    /// <summary>SpaceBetweenAlphabeticalWordのConfiguration</summary>
+    public record SpaceBetweenAlphabeticalWordConfiguration : ValidatorConfiguration
+    {
+        public SpaceBetweenAlphabeticalWordConfiguration(ValidationLevel level) : base(level)
+        {
+        }
+    }
+
+    // TODO: Validation対象に応じて、IDocumentValidatable, ISectionValidatable, ISentenceValidatableを実装する。
+    /// <summary>SpaceBetweenAlphabeticalWordのValidator</summary>
+    public class SpaceBetweenAlphabeticalWordValidator : Validator, ISentenceValidatable
+    {
+        /// <summary>Nlog</summary>
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        // TODO: 専用のValidatorConfigurationを別途定義する。
+        /// <summary>ValidatorConfiguration</summary>
+        public SpaceBetweenAlphabeticalWordConfiguration Config { get; init; }
+
+        // TODO: サポート対象言語がANYではない場合overrideで再定義する。
+        /// <summary></summary>
+        public override List<string> SupportedLanguages => new List<string>() { "ja-JP" };
+
+        // TODO: コンストラクタの引数定義は共通にすること。
+        public SpaceBetweenAlphabeticalWordValidator(
+            CultureInfo documentLangForTest,
+            SymbolTable symbolTable,
+            SpaceBetweenAlphabeticalWordConfiguration config) :
+            base(
+                config.Level,
+                documentLangForTest,
+                symbolTable)
+        {
+            this.Config = config;
+        }
+
+        public List<ValidationError> Validate(Sentence sentence)
+        {
+            List<ValidationError> result = new List<ValidationError>();
+
+            // validation
+
+            // TODO: MessageKey引数はErrorMessageにバリエーションがある場合にValidator内で条件判定して引数として与える。
+            result.Add(new ValidationError(
+                ValidationType.SpaceBetweenAlphabeticalWord,
+                this.Level,
+                sentence,
+                MessageArgs: new object[] { argsForMessageArg }));
+
+            return result;
+        }
+    }
+}
