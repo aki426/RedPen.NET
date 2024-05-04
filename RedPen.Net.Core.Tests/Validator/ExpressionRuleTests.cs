65ff0d180002460564809762125992e224ba8040
aki426 <aki426@outlook.jp>
2024-05-04 11:42:38 +0900

    test: 日本語の ExpressionRuleのテストケースを追加。

--- RedPen.Net.Core.Tests/Validator/ExpressionRuleTests.cs
+++ RedPen.Net.Core.Tests/Validator/ExpressionRuleTests.cs
@@ -61,5 +61,46 @@ namespace RedPen.Net.Core.Tests.Validator
             string.Join("|", matchedTokensList.Select(lis => string.Join("|", lis.Select(t => t.Surface))))
                 .Should().Contain(expected);
         }
+
+        /// <summary>ルール表現文字列から抽出したルールのマッチングテスト。</summary>
+        [Theory]
+        [InlineData("001", "本日+は", "本日は晴天なり。", 1, "本日|は")]
+        [InlineData("002", "本日+は", "本日は晴天なり、本日は曇天なり。", 2, "本日|は|本日|は")]
+        public void JapaneseMatchSurfaceTest(string nouse1, string rule, string text, int matchCount, string expected)
+        {
+            ExpressionRule expressionRule = ExpressionRuleExtractor.Run(rule);
+
+            output.WriteLine("★Rule...");
+            foreach (TokenElement token in expressionRule.Tokens)
+            {
+                output.WriteLine(token.ToString());
+            }
+            output.WriteLine("");
+
+            List<ImmutableList<TokenElement>> matchedTokensList = new List<ImmutableList<TokenElement>>();
+
+            Document doc = new PlainTextParserTests(output).GenerateDocument(text, "ja-JP");
+            foreach (var sentence in doc.GetAllSentences())
+            {
+                output.WriteLine("★Sentence...");
+                foreach (TokenElement token in sentence.Tokens)
+                {
+                    output.WriteLine(token.ToString());
+                }
+                output.WriteLine("");
+
+                //(bool isMatch, List<TokenElement> tokens1) =
+                (bool isMatch, List<ImmutableList<TokenElement>> tokens) value =
+                    expressionRule.MatchSurfaces(sentence.Tokens);
+
+                matchedTokensList.AddRange(value.tokens);
+            }
+
+            matchedTokensList.Count.Should().Be(matchCount);
+
+            // "|"ですべてのSurfaceを連結して比較する。
+            string.Join("|", matchedTokensList.Select(lis => string.Join("|", lis.Select(t => t.Surface))))
+                .Should().Contain(expected);
+        }
     }
 }
