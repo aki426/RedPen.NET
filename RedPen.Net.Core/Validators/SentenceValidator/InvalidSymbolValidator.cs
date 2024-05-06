4568044bedc04582c289d386223e3f69e1701cf2
aki426 <aki426@outlook.jp>
2024-05-06 21:50:32 +0900

    feat: InvalidSymbolのためのファイルを追加。

---
+++ RedPen.Net.Core/Validators/SentenceValidator/InvalidSymbolValidator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using NLog;
+using RedPen.Net.Core.Config;
+using RedPen.Net.Core.Model;
+
+namespace RedPen.Net.Core.Validators.SentenceValidator
+{
+    /// <summary>InvalidSymbolのConfiguration</summary>
+    public record InvalidSymbolConfiguration : ValidatorConfiguration
+    {
+        public InvalidSymbolConfiguration(ValidationLevel level) : base(level)
+        {
+        }
+    }
+
+    /// <summary>InvalidSymbolのValidator</summary>
+    public class InvalidSymbolValidator : Validator, ISentenceValidatable
+    {
+        /// <summary>Nlog</summary>
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>ValidatorConfiguration</summary>
+        public InvalidSymbolConfiguration Config { get; init; }
+
+        // TODO: コンストラクタの引数定義は共通にすること。
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidSymbolValidator"/> class.
+        /// </summary>
+        /// <param name="documentLangForTest">The document lang for test.</param>
+        /// <param name="symbolTable">The symbol table.</param>
+        /// <param name="config">The config.</param>
+        public InvalidSymbolValidator(
+            CultureInfo documentLangForTest,
+            SymbolTable symbolTable,
+            InvalidSymbolConfiguration config) :
+            base(
+                config.Level,
+                documentLangForTest,
+                symbolTable)
+        {
+            this.Config = config;
+        }
+
+        /// <summary>
+        /// Validate.
+        /// </summary>
+        /// <param name="sentence">The sentence.</param>
+        /// <returns>A list of ValidationErrors.</returns>
+        public List<ValidationError> Validate(Sentence sentence)
+        {
+            List<ValidationError> result = new List<ValidationError>();
+
+            // validation
+
+            // TODO: MessageKey引数はErrorMessageにバリエーションがある場合にValidator内で条件判定して引数として与える。
+            result.Add(new ValidationError(
+                ValidationType.InvalidSymbol,
+                this.Level,
+                sentence,
+                MessageArgs: new object[] { argsForMessageArg }));
+
+            return result;
+        }
+    }
+}
