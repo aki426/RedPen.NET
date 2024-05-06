0371745545170e23f731b5331bc8deefae5bc432
aki426 <aki426@outlook.jp>
2024-05-06 21:00:04 +0900

    feat: LongKanjiChain用のファイルを追加。

---
+++ RedPen.Net.Core/Validators/SentenceValidator/LongKanjiChainValidator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using NLog;
+using RedPen.Net.Core.Config;
+using RedPen.Net.Core.Model;
+
+namespace RedPen.Net.Core.Validators.SentenceValidator
+{
+    /// <summary>LongKanjiChainのConfiguration</summary>
+    public record LongKanjiChainConfiguration : ValidatorConfiguration
+    {
+        public LongKanjiChainConfiguration(ValidationLevel level) : base(level)
+        {
+        }
+    }
+
+    /// <summary>LongKanjiChainのValidator</summary>
+    public class LongKanjiChainValidator : Validator, ISentenceValidatable
+    {
+        /// <summary>Nlog</summary>
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>ValidatorConfiguration</summary>
+        public LongKanjiChainConfiguration Config { get; init; }
+
+        /// <summary></summary>
+        public override List<string> SupportedLanguages => new List<string>() { "ja-JP" };
+
+        // TODO: コンストラクタの引数定義は共通にすること。
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LongKanjiChainValidator"/> class.
+        /// </summary>
+        /// <param name="documentLangForTest">The document lang for test.</param>
+        /// <param name="symbolTable">The symbol table.</param>
+        /// <param name="config">The config.</param>
+        public LongKanjiChainValidator(
+            CultureInfo documentLangForTest,
+            SymbolTable symbolTable,
+            LongKanjiChainConfiguration config) :
+            base(
+                config.Level,
+                documentLangForTest,
+                symbolTable)
+        {
+            this.Config = config;
+        }
+
+        /// <summary>
+        /// Validate.
+        /// </summary>
+        /// <param name="sentence">The sentence.</param>
+        /// <returns>A list of ValidationErrors.</returns>
+        public List<ValidationError> Validate(Sentence sentence)
+        {
+            List<ValidationError> result = new List<ValidationError>();
+
+            // validation
+
+            // TODO: MessageKey引数はErrorMessageにバリエーションがある場合にValidator内で条件判定して引数として与える。
+            result.Add(new ValidationError(
+                ValidationType.LongKanjiChain,
+                this.Level,
+                sentence,
+                MessageArgs: new object[] { argsForMessageArg }));
+
+            return result;
+        }
+    }
+}
