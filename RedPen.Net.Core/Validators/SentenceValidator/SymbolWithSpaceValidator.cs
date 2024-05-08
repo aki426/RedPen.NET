5afe07965f65036541d8f43b1e8cada7d2dce5c1
aki426 <aki426@outlook.jp>
2024-05-08 22:50:38 +0900

    feat: SymbolWithSpaceのためのファイルを追加。

---
+++ RedPen.Net.Core/Validators/SentenceValidator/SymbolWithSpaceValidator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using NLog;
+using RedPen.Net.Core.Config;
+using RedPen.Net.Core.Model;
+
+namespace RedPen.Net.Core.Validators.SentenceValidator
+{
+    /// <summary>SymbolWithSpaceのConfiguration</summary>
+    public record SymbolWithSpaceConfiguration : ValidatorConfiguration
+    {
+        public SymbolWithSpaceConfiguration(ValidationLevel level) : base(level)
+        {
+        }
+    }
+
+    // TODO: Validation対象に応じて、IDocumentValidatable, ISectionValidatable, ISentenceValidatableを実装する。
+    /// <summary>SymbolWithSpaceのValidator</summary>
+    public class SymbolWithSpaceValidator : Validator, ISentenceValidatable
+    {
+        /// <summary>Nlog</summary>
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        // TODO: 専用のValidatorConfigurationを別途定義する。
+        /// <summary>ValidatorConfiguration</summary>
+        public SymbolWithSpaceConfiguration Config { get; init; }
+
+        // TODO: サポート対象言語がANYではない場合overrideで再定義する。
+        /// <summary></summary>
+        public override List<string> SupportedLanguages => new List<string>() { "ja-JP" };
+
+        // TODO: コンストラクタの引数定義は共通にすること。
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SymbolWithSpaceValidator"/> class.
+        /// </summary>
+        /// <param name="documentLangForTest">The document lang for test.</param>
+        /// <param name="symbolTable">The symbol table.</param>
+        /// <param name="config">The config.</param>
+        public SymbolWithSpaceValidator(
+            CultureInfo documentLangForTest,
+            SymbolTable symbolTable,
+            SymbolWithSpaceConfiguration config) :
+            base(
+                config.Level,
+                documentLangForTest,
+                symbolTable)
+        {
+            this.Config = config;
+        }
+
+        /// <summary>
+        /// Validate.
+        /// </summary>
+        /// <param name="sentence">The sentence.</param>
+        /// <returns>A list of ValidationErrors.</returns>
+        public List<ValidationError> Validate(Sentence sentence)
+        {
+            List<ValidationError> result = new List<ValidationError>();
+
+            // validation
+
+            // TODO: MessageKey引数はErrorMessageにバリエーションがある場合にValidator内で条件判定して引数として与える。
+            result.Add(new ValidationError(
+                ValidationType.SymbolWithSpace,
+                this.Level,
+                sentence,
+                MessageArgs: new object[] { argsForMessageArg }));
+
+            return result;
+        }
+    }
+}
