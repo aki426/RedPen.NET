a48043160c5e655b267ce2c7608dc88adb9253f9
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2024-06-02 08:36:51 +0900

    test: IDEのサポートを受けるため、テストプロジェクトを再作成。Configurationのテストケースを追加。

---
+++ RedPen.Net.CoreTests/Config/ConfigurationTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace RedPen.Net.Core.Config.Tests
+{
+    public class ConfigurationTests
+    {
+        [Fact()]
+        public void ConfigurationTest()
+        {
+            // デフォルトコンストラクタで生成された後、オブジェクト初期化子で初期化されなかった場合すべてデフォルト値となる。
+            var defaultConstructed = new Configuration();
+            defaultConstructed.DocumentLang.Should().Be("ja-JP");
+            defaultConstructed.Variant.Should().Be("");
+            defaultConstructed.MessageLang.Should().Be("ja-JP");
+            defaultConstructed.ValidatorConfigurations.Should().BeEmpty();
+            defaultConstructed.Symbols.Should().BeEmpty();
+
+            //var defaultConstructedWithDocumentLang = new Configuration() { DocumentLang = "en-US" };
+        }
+    }
+}
