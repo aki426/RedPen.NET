4121f63c368bfd513f380c8eb9014ff223cc2a6b
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2024-06-02 11:17:27 +0900

    test: Config/ConfigurationLoaderTest.csを新テストプロジェクトへ移植。グリーンバー。

--- RedPen.Net.CoreTests/Config/ConfigurationLoaderTests.cs
+++ RedPen.Net.CoreTests/Config/ConfigurationLoaderTests.cs
@@ -13,8 +13,11 @@
 //   limitations under the License.
 
 using System;
+using System.Globalization;
 using FluentAssertions;
 using RedPen.Net.Core.Globals;
+using RedPen.Net.Core.Validators.DocumentValidator;
+using RedPen.Net.Core.Validators.SentenceValidator;
 using Xunit;
 
 namespace RedPen.Net.Core.Config.Tests
@@ -98,6 +101,89 @@ namespace RedPen.Net.Core.Config.Tests
             conf.MessageLang.Should().Be("ja-JP");
             conf.ValidatorConfigurations.Should().BeEmpty();
             conf.Symbols.Should().BeEmpty();
+        }
+
+        /// <summary>
+        /// Loads the from json string test.
+        /// </summary>
+        [Fact]
+        public void LoadValidatorConfigurationAndSymbolsTest()
+        {
+            string jsonString = @"{
+    // コメントも書けます。
+    ""DocumentLang"": ""ja-JP"",
+    ""Variant"": ""zenkaku"",
+    ""ValidatorConfigurations"": [
+        {
+            ""Name"": ""SentenceLength"",
+            ""Level"" : ""ERROR"",
+            ""MinLength"": 120
+        },
+        {
+            ""Name"": ""JapaneseWordVariation"",
+            ""Level"" : ""INFO"",
+            ""WordMap"": {
+                ""node"": ""ノード"",
+                ""edge"": ""エッジ"",
+                ""graph"": ""グラフ"",
+                ""vertex"": ""バーテックス""
+            }
+        }
+    ],
+    ""Symbols"": [
+        {
+            ""Type"": ""COMMA"",
+            ""Value"": ""、"",
+            ""InvalidCharsStr"": "",，"",
+            ""NeedBeforeSpace"": false,
+            ""NeedAfterSpace"": true
+        },
+        {
+            ""Type"": ""FULL_STOP"",
+            ""Value"": ""。"",
+            ""InvalidCharsStr"": ""．"",
+            ""NeedBeforeSpace"": true,
+            ""NeedAfterSpace"": true
+        }
+    ]
+}";
+
+            // ライブラリ実装済みのValidatorConfiguration定義を与える。その定義にあるValidatorConfigurationのみロードされる。
+            var jsonLoader = new ConfigurationLoader(DefaultValidationDefinition.ValidatorConfTypeDefinitions);
+            var configuration = jsonLoader.Load(jsonString);
+
+            configuration.DocumentLang.Should().Be("ja-JP");
+            configuration.DocumentCultureInfo.Should().Be(CultureInfo.GetCultureInfo("ja-JP"));
+            configuration.Variant.Should().Be("zenkaku");
+
+            configuration.ValidatorConfigurations.Count.Should().Be(2);
+
+            configuration.ValidatorConfigurations[0].Should().BeOfType<SentenceLengthConfiguration>();
+            var sentenceLengthConfig = configuration.ValidatorConfigurations[0] as SentenceLengthConfiguration;
+            sentenceLengthConfig.Level.Should().Be(ValidationLevel.ERROR);
+            sentenceLengthConfig.MinLength.Should().Be(120);
+
+            configuration.ValidatorConfigurations[1].Should().BeOfType<JapaneseWordVariationConfiguration>();
+            var japaneseExpressionVariationConfig = configuration.ValidatorConfigurations[1] as JapaneseWordVariationConfiguration;
+            japaneseExpressionVariationConfig.Level.Should().Be(ValidationLevel.INFO);
+            japaneseExpressionVariationConfig.WordMap.Count.Should().Be(4);
+            japaneseExpressionVariationConfig.WordMap["node"].Should().Be("ノード");
+
+            configuration.Symbols.Count.Should().Be(2);
+            configuration.Symbols[0].Type.Should().Be(SymbolType.COMMA);
+            configuration.Symbols[0].Value.Should().Be('、');
+            configuration.Symbols[0].InvalidChars[0].Should().Be(',');
+            configuration.Symbols[0].InvalidChars[1].Should().Be('，');
+            configuration.Symbols[0].InvalidCharsStr.Should().Be(",，");
+            configuration.Symbols[0].NeedBeforeSpace.Should().BeFalse();
+            configuration.Symbols[0].NeedAfterSpace.Should().BeTrue();
+
+            configuration.Symbols[1].Type.Should().Be(SymbolType.FULL_STOP);
+            configuration.Symbols[1].Value.Should().Be('。');
+            configuration.Symbols[1].InvalidChars[0].Should().Be('．');
+            configuration.Symbols[1].InvalidCharsStr.Should().Be("．");
+            configuration.Symbols[1].NeedBeforeSpace.Should().BeTrue();
+            configuration.Symbols[1].NeedAfterSpace.Should().BeTrue();
         }
     }
 }
