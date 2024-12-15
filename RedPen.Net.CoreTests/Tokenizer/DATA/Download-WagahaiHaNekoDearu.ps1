3a87f047e2b5fbb7fc3a39b1695829ae1e956a14
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2024-12-15 19:39:50 +0900

    feat: テスト内容に仕様を修正。

--- RedPen.Net.CoreTests/Tokenizer/DATA/Download-WagahaiHaNekoDearu.ps1
+++ RedPen.Net.CoreTests/Tokenizer/DATA/Download-WagahaiHaNekoDearu.ps1
@@ -2,13 +2,10 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # ダウンロード先のURL
-$url = "https://www.aozora.gr.jp/cards/000148/files/789_ruby_5639.zip"
+$url = "https://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt"
 
-# ダウンロードしたZIPファイルの保存先パス
-$downloadPath = Join-Path $pwd "downloaded_file.zip"
-
-# 展開先のフォルダパス
-$extractPath = Join-Path $pwd "extracted_files"
+# ダウンロードしたファイルの保存先パス
+$downloadPath = Join-Path $pwd "neko.txt"
 
 try {
     Write-Host "ファイルをダウンロードしています..."
@@ -18,24 +15,7 @@ try {
     
     if (Test-Path $downloadPath) {
         Write-Host "ダウンロードが完了しました"
-
-        # 展開先フォルダが存在しない場合は作成
-        if (-not (Test-Path -Path $extractPath)) {
-            New-Item -ItemType Directory -Path $extractPath | Out-Null
-        }
-
-        # ZIPファイルを展開
-        Write-Host "ファイルを展開しています..."
-        Expand-Archive -Path $downloadPath -DestinationPath $extractPath -Force
-        Write-Host "展開が完了しました"
-
-        # ダウンロードしたZIPファイルを削除
-        Remove-Item -Path $downloadPath
-        Write-Host "一時ファイルを削除しました"
-
-        # 不要ファイル削除
-        Move-Item -Path (Join-Path $extractPath "wagahaiwa_nekodearu.txt") -Destination $pwd
-        Remove-Item -Path $extractPath
+        Write-Host "保存先: $downloadPath"
     }
     else {
         Write-Error "ファイルのダウンロードに失敗しました"
@@ -43,12 +23,6 @@ try {
 }
 catch {
     Write-Error "エラーが発生しました: $_"
-    
-    # エラーの詳細情報を表示
     Write-Host "エラーの詳細:" -ForegroundColor Red
     Write-Host $_.Exception.Message
-    if ($_.Exception.Response) {
-        Write-Host "ステータスコード: $($_.Exception.Response.StatusCode.Value__)"
-        Write-Host "ステータスの説明: $($_.Exception.Response.StatusDescription)"
-    }
 }
