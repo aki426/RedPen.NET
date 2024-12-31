b7e243a9f11dcdf76c233adbe1d1b2549b30887b
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2024-12-31 12:34:08 +0900

    test: 形容詞の活用変換検証コードを追加。動詞のコードを実際の形態素解析結果を使用するよう修正。

--- RedPen.Net.CoreTests/Inflection/InflectionResolverTests.cs
+++ RedPen.Net.CoreTests/Inflection/InflectionResolverTests.cs
@@ -32,42 +32,87 @@ namespace RedPen.Net.Core.Inflection.Tests
         }
 
         /// <summary>
-        /// 活用形Resolveのテスト。
+        /// 動詞の活用形Resolveのテスト。
         /// </summary>
         [Fact()]
-        public void ResolveTest()
+        public void ResolveVerbTest()
         {
-            TokenElement token = new TokenElement(
-                "笑わ",
-                "ワラワ",
-                "ワラワ",
-                ImmutableList.Create("動詞", "自立"),
-                "笑う",
-                "五段・ワ行促音便",
-                "未然形",
-                ImmutableList.Create(new LineOffset(1, 0))
-            );
-
-            InflectionResolver.Resolve(token, "基本形").surface.Should().Be("笑う");
-            InflectionResolver.Resolve(token, "未然形").surface.Should().Be("笑わ");
-            InflectionResolver.Resolve(token, "未然ウ接続").surface.Should().Be("笑お");
-            InflectionResolver.Resolve(token, "連用形").surface.Should().Be("笑い");
-            InflectionResolver.Resolve(token, "連用タ接続").surface.Should().Be("笑っ");
-            InflectionResolver.Resolve(token, "仮定形").surface.Should().Be("笑え");
-            InflectionResolver.Resolve(token, "命令ｅ").surface.Should().Be("笑え");
-
-            (bool success, string surface) result = InflectionResolver.Resolve(token, "存在しない活用形");
-            result.success.Should().BeFalse();
+            var tokenElements = KuromojiController.Tokenize(new Sentence("笑うらしい。", 1));
+            output.WriteLine("★Token...");
+            foreach (var item in tokenElements)
+            {
+                output.WriteLine(item.ToString());
+            }
+            output.WriteLine("");
 
             // TokenElement { S= "笑う",	R= "ワラウ(ワラウ)",	P= [動詞-自立],	I= 基本形/五段・ワ行促音便,	B= ,	O= (L1,0)-(L1,1)}
-            var token2 = KuromojiController.Tokenize(new Sentence("笑うらしい", 1)).FirstOrDefault();
-            InflectionResolver.Resolve(token2, "基本形").surface.Should().Be("笑う");
-            InflectionResolver.Resolve(token2, "未然形").surface.Should().Be("笑わ");
-            InflectionResolver.Resolve(token2, "未然ウ接続").surface.Should().Be("笑お");
-            InflectionResolver.Resolve(token2, "連用形").surface.Should().Be("笑い");
-            InflectionResolver.Resolve(token2, "連用タ接続").surface.Should().Be("笑っ");
-            InflectionResolver.Resolve(token2, "仮定形").surface.Should().Be("笑え");
-            InflectionResolver.Resolve(token2, "命令ｅ").surface.Should().Be("笑え");
+            var t = tokenElements[0];
+            InflectionResolver.Resolve(t, "基本形").surface.Should().Be("笑う");
+            InflectionResolver.Resolve(t, "未然形").surface.Should().Be("笑わ");
+            InflectionResolver.Resolve(t, "未然ウ接続").surface.Should().Be("笑お");
+            InflectionResolver.Resolve(t, "連用形").surface.Should().Be("笑い");
+            InflectionResolver.Resolve(t, "連用タ接続").surface.Should().Be("笑っ");
+            InflectionResolver.Resolve(t, "仮定形").surface.Should().Be("笑え");
+            InflectionResolver.Resolve(t, "命令ｅ").surface.Should().Be("笑え");
+
+            (bool success, string surface) result = InflectionResolver.Resolve(t, "存在しない活用形");
+            result.success.Should().BeFalse();
+        }
+
+        /// <summary>
+        /// 形容詞の活用形Resolveのテスト。
+        /// </summary>
+        [Fact()]
+        public void ResolveAdjTest()
+        {
+            var tokenElements = KuromojiController.Tokenize(new Sentence("四角い。", 1));
+            output.WriteLine("★Token...");
+            foreach (var item in tokenElements)
+            {
+                output.WriteLine(item.ToString());
+            }
+            output.WriteLine("");
+
+            var t = tokenElements[0];
+            InflectionResolver.Resolve(t, "基本形").surface.Should().Be("四角い");
+            InflectionResolver.Resolve(t, "未然ヌ接続").surface.Should().Be("四角から");
+            InflectionResolver.Resolve(t, "未然ウ接続").surface.Should().Be("四角かろ");
+            InflectionResolver.Resolve(t, "連用タ接続").surface.Should().Be("四角かっ");
+            InflectionResolver.Resolve(t, "連用テ接続").surface.Should().Be("四角くっ");
+            InflectionResolver.Resolve(t, "連用ゴザイ接続").surface.Should().Be("四角ぅ");
+            InflectionResolver.Resolve(t, "体言接続").surface.Should().Be("四角き");
+            InflectionResolver.Resolve(t, "仮定形").surface.Should().Be("四角けれ");
+            InflectionResolver.Resolve(t, "仮定縮約１").surface.Should().Be("四角けりゃ");
+            InflectionResolver.Resolve(t, "仮定縮約２").surface.Should().Be("四角きゃ");
+            InflectionResolver.Resolve(t, "命令ｅ").surface.Should().Be("四角かれ");
+            InflectionResolver.Resolve(t, "ガル接続").surface.Should().Be("四角");
+            InflectionResolver.Resolve(t, "文語基本形").surface.Should().Be("四角し");
+
+            (bool success, string surface) result = InflectionResolver.Resolve(t, "存在しない活用形");
+            result.success.Should().BeFalse();
+
+            tokenElements = KuromojiController.Tokenize(new Sentence("美しかった。", 1));
+            output.WriteLine("★Token...");
+            foreach (var item in tokenElements)
+            {
+                output.WriteLine(item.ToString());
+            }
+            output.WriteLine("");
+
+            t = tokenElements[0];
+            InflectionResolver.Resolve(t, "基本形").surface.Should().Be("美しい");
+            InflectionResolver.Resolve(t, "未然ヌ接続").surface.Should().Be("美しから");
+            InflectionResolver.Resolve(t, "未然ウ接続").surface.Should().Be("美しかろ");
+            InflectionResolver.Resolve(t, "連用タ接続").surface.Should().Be("美しかっ");
+            InflectionResolver.Resolve(t, "連用テ接続").surface.Should().Be("美しくっ");
+            InflectionResolver.Resolve(t, "連用ゴザイ接続").surface.Should().Be("美しゅぅ");
+            InflectionResolver.Resolve(t, "体言接続").surface.Should().Be("美しき");
+            InflectionResolver.Resolve(t, "仮定形").surface.Should().Be("美しけれ");
+            InflectionResolver.Resolve(t, "仮定縮約１").surface.Should().Be("美しけりゃ");
+            InflectionResolver.Resolve(t, "仮定縮約２").surface.Should().Be("美しきゃ");
+            InflectionResolver.Resolve(t, "命令ｅ").surface.Should().Be("美しかれ");
+            InflectionResolver.Resolve(t, "ガル接続").surface.Should().Be("美し");
+            InflectionResolver.Resolve(t, "文語基本形").surface.Should().Be("美し");
         }
     }
 }
