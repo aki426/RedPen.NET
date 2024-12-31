31286e75834576d112042d184cb2348905c8b6b4
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2024-12-31 17:54:18 +0900

    test: 「笑う」に関する常体敬体変換のテストケースグリーンバー。

--- RedPen.Net.CoreTests/NewFeatureResearch/常体敬体変換総引き当て表テスト.cs
+++ RedPen.Net.CoreTests/NewFeatureResearch/常体敬体変換総引き当て表テスト.cs
@@ -109,40 +109,16 @@ namespace RedPen.Net.CoreTests.NewFeatureResearch
             output.WriteLine("");
 
             // Tokenizeする
-            var jotaiTokens = KuromojiController.Tokenize(new Sentence(data.常体, 1));
-            var keitaiTokens = KuromojiController.Tokenize(new Sentence(data.敬体, 1));
+            var jotaiOrigin = KuromojiController.Tokenize(new Sentence(data.常体, 1));
+            var keitaiOrigin = KuromojiController.Tokenize(new Sentence(data.敬体, 1));
 
             // Token結果目視
             output.WriteLine("★Token...");
             output.WriteLine("常体");
-            foreach (var t in jotaiTokens) { output.WriteLine(t.ToString()); }
+            foreach (var t in jotaiOrigin) { output.WriteLine(t.ToString()); }
             output.WriteLine("敬体");
-            foreach (var t in keitaiTokens) { output.WriteLine(t.ToString()); }
+            foreach (var t in keitaiOrigin) { output.WriteLine(t.ToString()); }
             output.WriteLine("");
-
-            // 活用型と基本形の検証
-            if (jotaiTokens[0].PartOfSpeech[0] == "動詞")
-            {
-                jotaiTokens[0].InflectionType.Should().Be(data.常体活用型);
-                var (success5, surface5) = InflectionResolver.Resolve(jotaiTokens[0], "基本形");
-                success5.Should().BeTrue();
-                surface5.Should().Be(data.常体基本形);
-            }
-            else if (jotaiTokens[0].PartOfSpeech[0] == "名詞")
-            {
-                jotaiTokens[0].Surface.Should().Be(data.常体基本形);
-            }
-            if (keitaiTokens[0].PartOfSpeech[0] == "動詞")
-            {
-                keitaiTokens[0].InflectionType.Should().Be(data.敬体活用型);
-                var (success6, surface6) = InflectionResolver.Resolve(keitaiTokens[0], "基本形");
-                success6.Should().BeTrue();
-                surface6.Should().Be(data.敬体基本形);
-            }
-            else if (keitaiTokens[0].PartOfSpeech[0] == "名詞")
-            {
-                keitaiTokens[0].Surface.Should().Be(data.敬体基本形);
-            }
 
             // GrammarRuleを取得する
             output.WriteLine("★Rule...");
@@ -156,10 +132,37 @@ namespace RedPen.Net.CoreTests.NewFeatureResearch
 
             // 想定通りの形態素解析結果かを検証する
             // テストケースに記号が含まれるケースがあるので記号除去する。
-            var (success1, jotaiResultTokens) = jotaiRule.MatchExtendAtEnd(jotaiTokens.Where(x => x.PartOfSpeech[0] != "記号").ToImmutableList());
+            // NOTE: 以降も不要な要素を無視するためこの結果を利用する。
+            var (success1, jotaiTokens) = jotaiRule.MatchExtendAtEnd(jotaiOrigin.Where(x => x.PartOfSpeech[0] != "記号").ToImmutableList());
             success1.Should().BeTrue();
-            var (success2, keitaiResultTokens) = keitaiRule.MatchExtendAtEnd(keitaiTokens.Where(x => x.PartOfSpeech[0] != "記号").ToImmutableList());
+            var (success2, keitaiTokens) = keitaiRule.MatchExtendAtEnd(keitaiOrigin.Where(x => x.PartOfSpeech[0] != "記号").ToImmutableList());
             success2.Should().BeTrue();
+
+            // 活用型と基本形の検証
+            // 常体
+            if (jotaiTokens[0].PartOfSpeech[0] == "動詞")
+            {
+                jotaiTokens[0].InflectionType.Should().Be(data.常体活用型);
+                var (success5, surface5) = InflectionResolver.Resolve(jotaiTokens[0], "基本形");
+                success5.Should().BeTrue();
+                surface5.Should().Be(data.常体基本形);
+            }
+            else if (jotaiTokens[0].PartOfSpeech[0] == "名詞")
+            {
+                jotaiTokens[0].Surface.Should().Be(data.常体基本形);
+            }
+            // 敬体
+            if (keitaiTokens[0].PartOfSpeech[0] == "動詞")
+            {
+                keitaiTokens[0].InflectionType.Should().Be(data.敬体活用型);
+                var (success6, surface6) = InflectionResolver.Resolve(keitaiTokens[0], "基本形");
+                success6.Should().BeTrue();
+                surface6.Should().Be(data.敬体基本形);
+            }
+            else if (keitaiTokens[0].PartOfSpeech[0] == "名詞")
+            {
+                keitaiTokens[0].Surface.Should().Be(data.敬体基本形);
+            }
 
             ////////////////////////////////////////////////////////////////
             // 変換処理
@@ -172,32 +175,64 @@ namespace RedPen.Net.CoreTests.NewFeatureResearch
             }
 
             // 【常体→敬体】
-            output.WriteLine("【常体→敬体】");
+            output.WriteLine("順【常体→敬体】");
 
             if (data.変換 != "<")
             {
-                //// 活用解決
-                //string head = "";
-                //if (jotaiResultTokens[0].PartOfSpeech[0] == "動詞")
-                //{
-                //    var (success3, surface3) = InflectionResolver.Resolve(jotaiResultTokens[0], data.敬体活用形);
-                //    success3.Should().BeTrue();
-                //    head = surface3;
-                //}
-                //else if (jotaiResultTokens[0].PartOfSpeech[0] == "名詞")
-                //{
-                //    head = jotaiResultTokens[0].Surface;
-                //}
+                // 活用解決
+                string head = "";
+                if (jotaiTokens[0].PartOfSpeech[0] == "動詞")
+                {
+                    var (success3, surface3) = InflectionResolver.Resolve(jotaiTokens[0], data.敬体活用形);
+                    success3.Should().BeTrue();
+                    head = surface3;
+                }
+                else if (jotaiTokens[0].PartOfSpeech[0] == "名詞")
+                {
+                    head = jotaiTokens[0].Surface;
+                }
 
-                //// 語尾付与＋想定文言と合致するかどうかを検証
-                //output.WriteLine("★Result...");
-                //string resultStr = $"{head}{data.敬体語尾}";
-                //output.WriteLine(resultStr);
-                //resultStr.Should().Be(data.敬体);
+                // 語尾付与＋想定文言と合致するかどうかを検証
+                output.WriteLine("★Result...");
+                string resultStr = $"{head}{data.敬体語尾}";
+                output.WriteLine(resultStr);
+                // 記号や前置されるTokenがある場合を考慮して、マッチした部分のみで検証する。
+                resultStr.Should().Be(string.Join("", keitaiTokens.Select(i => i.Surface)));
             }
             else
             {
                 output.WriteLine("★常体→敬体変換スキップ...");
+                output.WriteLine("");
+            }
+
+            // 【敬体→常体】
+            output.WriteLine("逆【敬体→常体】");
+
+            if (data.変換 != "<")
+            {
+                // 活用解決
+                string head = "";
+                if (keitaiTokens[0].PartOfSpeech[0] == "動詞")
+                {
+                    var (success3, surface3) = InflectionResolver.Resolve(keitaiTokens[0], data.常体活用形);
+                    success3.Should().BeTrue();
+                    head = surface3;
+                }
+                else if (keitaiTokens[0].PartOfSpeech[0] == "名詞")
+                {
+                    head = keitaiTokens[0].Surface;
+                }
+
+                // 語尾付与＋想定文言と合致するかどうかを検証
+                output.WriteLine("★Result...");
+                string resultStr = $"{head}{data.常体語尾}";
+                output.WriteLine(resultStr);
+                // 記号や前置されるTokenがある場合を考慮して、マッチした部分のみで検証する。
+                resultStr.Should().Be(string.Join("", jotaiTokens.Select(i => i.Surface)));
+            }
+            else
+            {
+                output.WriteLine("★敬体→常体変換スキップ...");
                 output.WriteLine("");
             }
         }
